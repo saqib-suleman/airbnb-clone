3cc17d424cc66db753c14fa3da27a24bded1cdad
Saqib Suleman <saqibs3291@gmail.com>
2023-07-31 00:21:56 +0500

    feat: add logo in navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import type { Metadata } from "next";
+import Navbar from "./components/Navbar/Navbar";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
