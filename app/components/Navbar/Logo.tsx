51c17de2ec926c6d61777e0c9d85a5a9849fc869
Saqib Suleman <saqibs3291@gmail.com>
2023-08-06 00:11:45 +0500

    feat: add categories

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -8,6 +8,7 @@ function Logo() {
 
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="hidden md:block cursor-pointer"
       height="100"
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 type NavbarProps = {
   currentUser?: SafeUser | null;
@@ -24,6 +25,7 @@ function Navbar({currentUser}: NavbarProps) {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 }
