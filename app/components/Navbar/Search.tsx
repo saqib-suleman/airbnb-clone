56babc77ab4fd70fa62f4b3a449551dea5917ec9
Saqib Suleman <saqibs3291@gmail.com>
2023-08-12 21:17:21 +0500

    feat: finalize app

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,18 +1,102 @@
-"use client";
-import useSearchModal from "@/app/hooks/useSearchModal";
-import { BiSearch } from "react-icons/bi";
+'use client';
 
-function Search() {
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { differenceInDays } from 'date-fns';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useCountries from '@/app/hooks/useCountries';
+
+const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
 
-  return (
-    <div onClick={searchModal.onOpen} className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadhow-md transition cursor-pointer">
-      <div className="flex flex-row items-center jusitfy-between">
-        <div className="text-sm font-semibold px-6">Anywhere</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Any Week
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
+  return ( 
+    <div
+      onClick={searchModal.onOpen}
+      className="
+        border-[1px] 
+        w-full 
+        md:w-auto 
+        py-2 
+        rounded-full 
+        shadow-sm 
+        hover:shadow-md 
+        transition 
+        cursor-pointer
+      "
+    >
+      <div 
+        className="
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+        "
+      >
+        <div 
+          className="
+            text-sm 
+            font-semibold 
+            px-6
+          "
+        >
+          {locationLabel}
         </div>
-        <div
+        <div 
+          className="
+            hidden 
+            sm:block 
+            text-sm 
+            font-semibold 
+            px-6 
+            border-x-[1px] 
+            flex-1 
+            text-center
+          "
+        >
+          {durationLabel}
+        </div>
+        <div 
           className="
             text-sm 
             pl-6 
@@ -21,10 +105,18 @@ function Search() {
             flex 
             flex-row 
             items-center 
-            gap-3"
+            gap-3
+          "
         >
-          <div className="hidden sm:block">Add Guests</div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
+          <div className="hidden sm:block">{guestLabel}</div>
+          <div 
+            className="
+              p-2 
+              bg-rose-500 
+              rounded-full 
+              text-white
+            "
+          >
             <BiSearch size={18} />
           </div>
         </div>
@@ -32,5 +124,5 @@ function Search() {
     </div>
   );
 }
-
+ 
 export default Search;
